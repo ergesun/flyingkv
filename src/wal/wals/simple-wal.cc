0bc1ebff78458b4db0e5ddf23a3c4a2a0ff5623b
ErgeSun <maxlovexxx@163.com>
2018-10-31 20:01:01 +0800

    optimize wal

--- src/wal/wals/simple-wal.cc
+++ src/wal/wals/simple-wal.cc
@@ -37,9 +37,9 @@ SimpleWal::SimpleWal(std::string &rootDir, common::EntryCreateHandler &&entryCre
 }
 
 SimpleWal::~SimpleWal() {
-    if (-1 != m_iFd) {
-        close(m_iFd);
-        m_iFd = -1;
+    if (-1 != m_fd) {
+        close(m_fd);
+        m_fd = -1;
     }
 }
 
@@ -52,17 +52,21 @@ uint64_t SimpleWal::AppendEntry(common::IEntry *entry) {
         LOGFFUN << "simple wal encode entry error";
     }
 
-    auto walEntryStartOffset = m_iFileSize;
+    auto walEntryStartOffset = m_fileSize;
     auto rawEntrySize = eb->AvailableLength();
-    auto walEntrySize = rawEntrySize + 8/*len 4 + start pos 4*/;
+    auto walEntrySize = rawEntrySize + SMLOG_ENTRY_EXTRA_FIELDS_SIZE;
     // mpo will be Putted in Buffer 'b'.
-    auto mpo = common::g_pMemPool->Get(walEntrySize);
+    auto mpo = common::g_pMemPool->Get(uint32_t(walEntrySize));
     auto bufferStart = (uchar*)(mpo->Pointer());
     common::Buffer wb;
     wb.Refresh(bufferStart, bufferStart + walEntrySize - 1, bufferStart, bufferStart + walEntrySize - 1, mpo);
     // size
     ByteOrderUtils::WriteUInt32(wb.GetPos(), uint32_t(rawEntrySize));
-    wb.SetPos(wb.GetPos() + 4);
+    wb.SetPos(wb.GetPos() + SMLOG_SIZE_LEN);
+
+    // log id
+    ByteOrderUtils::WriteUInt64(wb.GetPos(), uint32_t(m_currentLogIdx));
+    wb.SetPos(wb.GetPos() + SMLOG_ENTRY_ID_LEN);
 
     // content
     memcpy(eb->GetPos(), wb.GetPos(), size_t(rawEntrySize));
@@ -71,11 +75,12 @@ uint64_t SimpleWal::AppendEntry(common::IEntry *entry) {
     // offset
     ByteOrderUtils::WriteUInt32(wb.GetPos(), walEntryStartOffset);
 
-    WriteFileFullyWithFatalLOG(m_iFd, (char*)(wb.GetStart()), walEntrySize, m_sLogFilePath.c_str());
-    FDataSyncFileWithFatalLOG(m_iFd, m_sLogFilePath.c_str());
-    m_iFileSize += walEntrySize;
-    auto entryId = m_iCurrentLogIdx;
-    m_mpEntriesIdEndOffset[m_iCurrentLogIdx++] = m_iFileSize - 1;
+    WriteFileFullyWithFatalLOG(m_fd, (char*)(wb.GetStart()), walEntrySize, m_sLogFilePath.c_str());
+    FDataSyncFileWithFatalLOG(m_fd, m_sLogFilePath.c_str());
+    m_fileSize += walEntrySize;
+    auto entryId = m_currentLogIdx;
+    m_mpEntriesIdEndOffset[m_currentLogIdx] = m_fileSize - 1;
+    increase_log_id();
     return entryId;
 }
 
@@ -85,7 +90,7 @@ std::vector<WalEntry> SimpleWal::Load() {
     std::vector<WalEntry> rs;
     if (!utils::FileUtils::Exist(m_sLogFilePath)) {
         LOGDFUN2("create sm log file ", m_sLogFilePath.c_str());
-        if (-1 == (m_iFd = utils::FileUtils::Open(m_sLogFilePath, O_WRONLY, O_CREAT, 0644))) {
+        if (-1 == (m_fd = utils::FileUtils::Open(m_sLogFilePath, O_WRONLY, O_CREAT, 0644))) {
             auto err = errno;
             LOGFFUN << "create sm log file " << m_sLogFilePath.c_str() << " failed with errmsg " << strerror(err);
         }
@@ -93,31 +98,31 @@ std::vector<WalEntry> SimpleWal::Load() {
         // write file header.
         uchar header[SMLOG_MAGIC_NO_LEN];
         ByteOrderUtils::WriteUInt32(header, SMLOG_MAGIC_NO);
-        WriteFileFullyWithFatalLOG(m_iFd, (char*)header, SMLOG_MAGIC_NO_LEN, m_sLogFilePath.c_str());
+        WriteFileFullyWithFatalLOG(m_fd, (char*)header, SMLOG_MAGIC_NO_LEN, m_sLogFilePath.c_str());
     } else {
-        if (-1 == (m_iFd = utils::FileUtils::Open(m_sLogFilePath, O_RDWR, 0, 0))) {
+        if (-1 == (m_fd = utils::FileUtils::Open(m_sLogFilePath, O_RDWR, 0, 0))) {
             auto err = errno;
             LOGFFUN << "open sm log file " << m_sLogFilePath.c_str() << " failed with errmsg " << strerror(err);
         }
 
         // 1. check if file is empty.
-        auto fileSize = utils::FileUtils::GetFileSize(m_iFd);
+        auto fileSize = utils::FileUtils::GetFileSize(m_fd);
         if (-1 == fileSize) {
             auto err = errno;
             LOGFFUN << "get file size for " << m_sLogFilePath.c_str() << " failed with errmsg " << strerror(err);
         }
-        if (0 == (m_iFileSize = (uint32_t)fileSize)) {
+        if (0 == (m_fileSize = (uint32_t)fileSize)) {
             LOGDFUN3("sm log file ", m_sLogFilePath.c_str(), " is empty!");
             return std::vector<WalEntry>();
         }
 
         // 2. check if file header is corrupt.
-        if (m_iFileSize < SMLOG_MAGIC_NO_LEN) {
+        if (m_fileSize < SMLOG_MAGIC_NO_LEN) {
             LOGFFUN << "sm log file " << m_sLogFilePath.c_str() << " header is corrupt!";
         }
 
         // TODO(sunchao): 如果以后扩展这个wal，支持大量的logs，就不要mmap了
-        auto mapRet = mmap(nullptr, (size_t)m_iFileSize, PROT_READ, MAP_PRIVATE, m_iFd, 0);
+        auto mapRet = mmap(nullptr, (size_t)m_fileSize, PROT_READ, MAP_PRIVATE, m_fd, 0);
         if (MAP_FAILED == mapRet) {
             auto err = errno;
             LOGFFUN << "mmap sm log " << m_sLogFilePath.c_str() << " failed with errmsg " << strerror(err);
@@ -133,15 +138,16 @@ std::vector<WalEntry> SimpleWal::Load() {
         offset += SMLOG_MAGIC_NO_LEN;
         common::Buffer b;
         // 3. load sm log content
-        while (offset <= m_iFileSize - 1) {
+        while (offset <= m_fileSize - 1) {
             // 3.1 read sm log len
             auto len = ByteOrderUtils::ReadUInt32(buf + offset);
-            auto startPos = ByteOrderUtils::ReadUInt32(buf + offset + len);
+            auto startPos = ByteOrderUtils::ReadUInt32(buf + offset + SMLOG_ENTRY_ID_LEN + len);
             if (startPos != offset) {
                 LOGFFUN << "parse sm log " << m_sLogFilePath.c_str() << " failed at offset " << offset;
             }
 
-            auto startPtr = buf + offset;
+            auto entryId = ByteOrderUtils::ReadUInt64(buf + offset + SMLOG_SIZE_LEN);
+            auto startPtr = buf + offset + SMLOG_SIZE_LEN + SMLOG_ENTRY_ID_LEN;
             auto endPtr = buf + len - 1;
             b.Refresh(startPtr, endPtr, startPtr, endPtr, nullptr);
             auto entry = m_entryCreator();
@@ -149,61 +155,65 @@ std::vector<WalEntry> SimpleWal::Load() {
                 LOGFFUN << "deserialize sm log entry at offset " << offset << " failed!";
             }
 
-            auto we = WalEntry(m_iCurrentLogIdx, entry);
+            auto we = WalEntry(entryId, entry);
             rs.push_back(std::move(we));
-            offset += (len + sizeof(uint32_t));
-            m_mpEntriesIdEndOffset[m_iCurrentLogIdx++] = offset - 1;
+            offset += (len + SMLOG_ENTRY_EXTRA_FIELDS_SIZE);
+            m_mpEntriesIdEndOffset[entryId] = offset - 1;
         }
 
-        if (-1 == munmap(mapRet, m_iFileSize)) {
+        if (-1 == munmap(mapRet, m_fileSize)) {
             auto err = errno;
             LOGFFUN << "munmap file " << m_sLogFilePath.c_str() << " failed with errmsg " << strerror(err);
         }
     }
 
-    LSeekFileWithFatalLOG(m_iFd, 0, SEEK_END, m_sLogFilePath.c_str());
+    LSeekFileWithFatalLOG(m_fd, 0, SEEK_END, m_sLogFilePath.c_str());
     return rs;
 }
 
 bool SimpleWal::TruncateAhead(uint64_t id) {
+    if (UNLIKELY(!m_bLoaded)) {
+        LOGFFUN << "simple wal has not been loaded.";
+    }
+
     if (m_mpEntriesIdEndOffset.end() == m_mpEntriesIdEndOffset.find(id)) {
         return false;
     }
 
-    if (0 == m_iFileSize) {
+    if (0 == m_fileSize) {
         return true;
     }
 
     auto endOffset = m_mpEntriesIdEndOffset[id];
     auto offset = endOffset + 1;
-    if (offset == m_iFileSize) {
-        if (-1 == ftruncate(m_iFd, 4/*reserve header*/)) {
+    if (offset == m_fileSize) {
+        if (-1 == ftruncate(m_fd, SMLOG_MAGIC_NO_LEN/*reserve header*/)) {
             LOGFFUN << "simple wal clean failed with errmsg " << strerror(errno);
         }
-        m_iFileSize = 0;
+        m_fileSize = 0;
         m_mpEntriesIdEndOffset.clear();
 
         return true;
     }
 
-    auto leftBufLen = m_iFileSize - offset;
+    auto leftBufLen = m_fileSize - offset;
     auto buf = new char[leftBufLen];
-    LSeekFileWithFatalLOG(m_iFd, offset, SEEK_SET, m_sLogFilePath.c_str());
-    if (-1 == utils::IOUtils::ReadFully_V2(m_iFd, &buf, size_t(leftBufLen))) {
+    LSeekFileWithFatalLOG(m_fd, offset, SEEK_SET, m_sLogFilePath.c_str());
+    if (-1 == utils::IOUtils::ReadFully_V2(m_fd, &buf, size_t(leftBufLen))) {
         LOGFFUN << "simple wal " << m_sLogFilePath.c_str() << " read error " << strerror(errno);
     }
 
-    LSeekFileWithFatalLOG(m_iFd, SMLOG_MAGIC_NO_LEN, SEEK_SET, m_sLogFilePath.c_str());
+    LSeekFileWithFatalLOG(m_fd, SMLOG_MAGIC_NO_LEN, SEEK_SET, m_sLogFilePath.c_str());
     // TODO(sunchao): 以下俩文件操作不是原子的，如果过程中挂了，会导致不一致。先这样，有时间加个meta并且通过临时文件重命名的方式来做。
-    if (-1 == utils::IOUtils::WriteFully(m_iFd, buf, size_t(leftBufLen))) {
+    if (-1 == utils::IOUtils::WriteFully(m_fd, buf, size_t(leftBufLen))) {
         LOGFFUN << "simple wal " << m_sLogFilePath.c_str() << " write error " << strerror(errno);
     }
 
-    if (-1 == ftruncate(m_iFd, 0)) {
+    if (-1 == ftruncate(m_fd, 0)) {
         LOGFFUN << "simple wal truncate ahead failed with errmsg " << strerror(errno);
     }
 
-    m_iFileSize = uint32_t(SMLOG_MAGIC_NO_LEN + leftBufLen);
+    m_fileSize = uint32_t(SMLOG_MAGIC_NO_LEN + leftBufLen);
     // TODO(sunchao): 此处需要优化，有两点：1. id无限增长的话会导致溢出，可以通过多文件的方式解决。
     //                                   2. 每次truncate都需要从0开始清理这个map不好，需要记录last清理id
     for (uint32_t i = 0; i <= id; ++i) {
@@ -214,11 +224,11 @@ bool SimpleWal::TruncateAhead(uint64_t id) {
 }
 
 void SimpleWal::Reset() {
-    if (-1 == ftruncate(m_iFd, 0)) {
+    if (-1 == ftruncate(m_fd, 0)) {
         LOGFFUN << "simple wal clean failed with errmsg " << strerror(errno);
     }
-    m_iCurrentLogIdx = 0;
-    m_iFileSize = 0;
+    m_currentLogIdx = 0;
+    m_fileSize = 0;
     m_mpEntriesIdEndOffset.clear();
 }
 }
--- src/wal/wals/simple-wal.h
+++ src/wal/wals/simple-wal.h
@@ -9,11 +9,17 @@
 #include <unordered_map>
 
 #include "../iwal.h"
+#include "../../sys/gcc-buildin.h"
 
-//         SMLOG_MAGIC_NO        s m l g
-#define    SMLOG_MAGIC_NO      0x736d6c67
-#define    SMLOG_MAGIC_NO_LEN  4
-#define    SMLOG_PREFIX_NAME   "smlog"
+//         SMLOG_MAGIC_NO                   s m l g
+#define    SMLOG_MAGIC_NO                 0x736d6c67
+#define    SMLOG_MAGIC_NO_LEN             4
+#define    SMLOG_PREFIX_NAME              "smlog"
+
+#define    SMLOG_START_POS_LEN            4
+#define    SMLOG_SIZE_LEN                 4
+#define    SMLOG_ENTRY_ID_LEN             8
+#define    SMLOG_ENTRY_EXTRA_FIELDS_SIZE  16 // contentLen(4) + logId(8) + startPos(4)
 
 namespace flyingkv {
 namespace common {
@@ -23,11 +29,13 @@ namespace wal {
 // TODO(sunchao): 1. add batch sync feature with autoSync conf? 2. with multi files?
 /**
  * Not thread-safe!
- * log format on disk file(file header + log segments)：
- *  |<-log file header(magic no)->|
- *  |<-log content len(u32)->|<-log content->|<-log segment start pos(u32)->|
- *   ...(log segments)...
- *  |<-log content len(u32)->|<-log content->|<-log segment start pos(u32)->|
+ * CASE 'LOG format version == 1':
+ *   log id = [log version(u32) + entry id(u32)]
+ *   log format on disk file(file header + log segments)：
+ *    |<-log file header(magic no)->|
+ *    |<-log content len(u32)->|<-log format verion(u8)->|<-log entry id(u64)->|<-log content->|<-log segment start pos(u32)->|
+ *     ...(log segments)...
+ *    |<-log content len(u32)->|<-log format verion(u8)->|<-log entry id(u64)->|<-log content->|<-log segment start pos(u32)->|
  */
 class SimpleWal : public IWal {
 PUBLIC
@@ -40,14 +48,21 @@ PUBLIC
     void Reset() override;
 
 PRIVATE
+    void load_log_version();
+
+PRIVATE
     std::string                           m_sRootDir;
     std::unordered_map<uint64_t, int64_t> m_mpEntriesIdEndOffset;
-    uint64_t                              m_iCurrentLogIdx;
+    // 增长时无需考虑溢出，因为没等溢出version就会增长
+    uint64_t                              m_currentLogIdx;
+    uint64_t                              m_lastTruncLogIdx;
     std::string                           m_sLogFilePath;
-    int                                   m_iFd           = -1;
-    uint32_t                              m_iFileSize     = 0;
+    int                                   m_fd             = -1;
+    uint32_t                              m_fileSize       = 0;
     common::EntryCreateHandler            m_entryCreator;
-    bool                                  m_bLoaded       = false;
+    bool                                  m_bLoaded        = false;
+    // 增长时无需考虑溢出，能活到那么久么。。。如果真活到那么久，由于日志有version，可以到时候扩展为64位的，甚至扩展为时间戳
+    uint32_t                              m_logFileVersion = 0;
 };
 }
 }
